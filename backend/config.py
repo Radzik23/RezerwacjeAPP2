c0793a1c3e4b24b4c7cca0517f42f7df50ad630b
Dominika <129169321+dominiquelt@users.noreply.github.com>
2025-05-19 15:16:57 +0200

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///acha.db")  # Możesz podmienić później na PostgreSQL
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///acha.db")  #
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "tajny-klucz")
